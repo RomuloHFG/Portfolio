20f6ee0b09417bc9bf398f136be8b97d33eac9d8
Rômulo Henrique Ferreira Gonçalves <157440587+Romulo-HFG@users.noreply.github.com>
2024-02-02 23:15:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text", {
+
+    strings: [
+        'Full-Stack Developer.', 
+        
+    ],
+    stringsElement: null,
+    typeSpeed: 100,
+    startDelay: 1000,
+    backSpeed: 100,
+loop: true
+});
